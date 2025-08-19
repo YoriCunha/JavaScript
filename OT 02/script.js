4532ed366ba48f663ffaaebace98275e211fe74a
YoriCunha <yori_cunha@estudante.sc.senai.br>
2025-08-19 19:47:20 -0300

    up

--- OT 02/script.js
+++ OT 02/script.js
@@ -1,1 +1,26 @@
-let animais = ["Babuino", "Gato", "Cachorro"];
+let animais = [];
+
+animais.push("Cachorro");
+animais.push("Gato");
+animais.push("Coelho");
+animais.push("Tartaruga");
+
+animais.shift(); 
+console.log(animais); 
+
+animais.unshift("Peixe", "Papagaio");
+console.log(animais);
+
+animais[1] = "Girafa";
+console.log(animais);   
+console.log("Total de animais:", animais.length);
+
+
+let frutas = ["Maçã", "Banana", "Laranja","Uva"];
+console.log(frutas);
+console.log("Total de frutas:",frutas.length);
+
+
+for(let i = 0; i < frutas.length; i++) {
+    console.log(frutas[i]);
+}
