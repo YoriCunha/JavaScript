d97f9e44e46fb8266ce1d669d678f22b8dde23f6
YORI GANDOLFI DA CUNHA <yori_cunha@estudante.sc.senai.br>
2025-08-25 20:52:27 -0300

    up

---
+++ OT 04/script.js
@@ -0,0 +1,12 @@
+
+let age = 65;
+
+if (age < 13) {
+    console.log("Criança")
+} else if (age >= 13 && age <= 17) {
+    console.log("Adolescente")
+} else if (age >= 18 && age <= 64) {
+    console.log("Aduto")
+} else if (age >= 65) {
+    console.log("Velho")
+}
