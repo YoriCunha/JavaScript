ab4da9b597f7a4b5dd73b600968237d2c2996a0d
YORI GANDOLFI DA CUNHA <yori_cunha@estudante.sc.senai.br>
2025-08-25 21:11:06 -0300

    up

--- OT 04/script.js
+++ OT 04/script.js
@@ -1,12 +1,31 @@
 
-let age = 65;
+// let age = 65;
 
-if (age < 13) {
-    console.log("Criança")
-} else if (age >= 13 && age <= 17) {
-    console.log("Adolescente")
-} else if (age >= 18 && age <= 64) {
-    console.log("Aduto")
-} else if (age >= 65) {
-    console.log("Velho")
+// if (age < 13) {
+//     console.log("Criança")
+// } else if (age >= 13 && age <= 17) {
+//     console.log("Adolescente")
+// } else if (age >= 18 && age <= 64) {
+//     console.log("Aduto")
+// } else if (age >= 65) {
+//     console.log("Velho")
+// }
+
+let number = prompt("Digite um numero: ");
+switch (number) {
+    case "1":
+        alert("Você errou!");
+        break;
+
+    case "2":
+        alert("Você errou!");
+        break;
+
+    case "3":
+        alert("Você errou!");
+        break;
+
+    case "4":
+        alert("Você acertou!");
+        break;
 }
