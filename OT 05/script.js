8ad97b2cb3bc24b7445f2ba006527035614b25ec
YoriCunha <yori_cunha@estudante.sc.senai.br>
2025-08-27 21:20:46 -0300

    up

--- OT 05/script.js
+++ OT 05/script.js
@@ -1,3 +1,47 @@
-function somar(num1, num2){
-    return num1 + num2;
-}
+// function somar(num1, num2){
+//     return num1 + num2;
+// }
+
+// let result = somar(5, 10);
+// alert("O resultado da soma é: " + result);
+
+// function saudar(nome){
+//     alert("Olá, " + nome + "! Seja bem-vindo(a)!");
+// }
+// saudar("Maria");
+
+// let soma = (a, b) => a + b;
+// let num1 = prompt("Digite a largura do retangulo: ");
+// let num2 = prompt("Digite a altura do retangulo: ");
+// function area(num1, num2){
+//     alert("A área do retangulo é: " + num1 * num2) ;
+// }
+// area(num1, num2);
+
+// let numero = prompt("Digite um número para verificar se é par ou ímpar: ");
+// function parImpar(numero){
+//     if(numero % 2 === 0){
+//         alert("O número " + numero + " é par.");
+//     } else {
+//         alert("O número " + numero + " é ímpar.");
+//     }
+// }
+// parImpar(numero);
+
+// let polegadas = prompt("Digite o valor em polegadas: ");
+// function converterPolegadas(polegadas){
+//     let centimetros = polegadas * 2.54;
+//     alert(polegadas + " polegadas é igual a " + centimetros + " centímetros.");
+// }
+// converterPolegadas(polegadas);
+
+// let peso = prompt("Digite o peso: ");
+// let altura = prompt("Digite a altura: ");
+// function calcularIMC(peso, altura){
+//     let imc = peso / (altura * altura);
+//     alert("Seu IMC é: " + imc.toFixed(1));
+// }
+// calcularIMC(peso, altura);
+
+
+
